cd92bc0d6cff1e1f27c6398ed959a7dec5aab93c
kalmonJ <kalmonkk69@gmail.com>
2023-06-09 22:28:18 -0300

    fix: clear assert

--- src/lib/assert.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloServer } from "@apollo/server";
-import { ServerContext } from "./context";
-
-export function assertServer(
-  value: ApolloServer<ServerContext> | null
-): asserts value is ApolloServer<ServerContext> {
-  if (!value) throw new Error("Could not load server!");
-}
