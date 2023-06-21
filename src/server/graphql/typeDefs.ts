38d27c3628dfa3aef2e228b99a2171ab11e56f1c
kalmonJ <kalmonkk69@gmail.com>
2023-06-21 20:57:28 -0300

    fix: schema path

--- src/server/graphql/typeDefs.ts
+++ src/server/graphql/typeDefs.ts
@@ -1,11 +1,14 @@
 import path from "path";
 import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import "./modules";
 
 export const typeDefs = loadSchemaSync(
   path.join(
     __dirname,
-    "../../../../../src/server/graphql/modules/**/*.graphql"
+    process.env.NODE_ENV !== "development"
+      ? "./modules/**/*.graphql"
+      : "../../../../../src/server/graphql/modules/**/*.graphql"
   ),
   {
     loaders: [new GraphQLFileLoader()],
