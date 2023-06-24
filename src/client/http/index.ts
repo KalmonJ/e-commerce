ec35e1e898549ea03c231135495be72b75705e17
kalmonJ <kalmonkk69@gmail.com>
2023-06-24 13:30:14 -0300

    fix: QA domain

--- src/client/http/index.ts
+++ src/client/http/index.ts
@@ -25,7 +25,6 @@ export const GraphQLRequest = (graphqlEndpoint: string) => {
         return serverResponse.data as TRes;
       } catch (err: any) {
         console.log(err);
-
         throw new Error(err.message);
       }
     },
